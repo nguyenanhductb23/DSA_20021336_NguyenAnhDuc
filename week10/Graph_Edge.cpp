0d662b024e2c3e2c18a37b366e2157eb3831cf6a
nguyenanhductb23 <78080392+nguyenanhductb23@users.noreply.github.com>
2021-11-12 21:24:17 +0700

    Week10 - Part2

--- week10/Graph_Edge.cpp
+++ week10/Graph_Edge.cpp
@@ -32,10 +32,13 @@ struct Node {
     }
 };
 
-void makeEdge(Node* v1, Node* v2) {
-    v1->insertNext(v2);
-    v2->insertNext(v1);
-}
+struct Edge {
+    Edge() {}
+    Edge(Node* v1, Node* v2) {
+        v1->insertNext(v2);
+        v2->insertNext(v1);
+    }
+};
 
 struct Graph {
     map<Node*, bool> unpassed;
@@ -45,17 +48,16 @@ struct Graph {
         for (int i = 0; i < 8; i++) {
             n.push_back(new Node(i));
         }
-        makeEdge(n[1], n[2]);
-        makeEdge(n[1], n[4]);
-        makeEdge(n[1], n[5]);
-        makeEdge(n[3], n[2]);
-        makeEdge(n[4], n[2]);
-        makeEdge(n[3], n[4]);
-      //makeEdge(n[0], n[3]);
-        makeEdge(n[5], n[4]);
-        makeEdge(n[3], n[6]);
-        makeEdge(n[3], n[7]);
-        makeEdge(n[0], n[3]);
+        edges.push_back(new Edge(n[1], n[2]));
+        edges.push_back(new Edge(n[1], n[4]));
+        edges.push_back(new Edge(n[1], n[5]));
+        edges.push_back(new Edge(n[3], n[2]));
+        edges.push_back(new Edge(n[4], n[2]));
+        edges.push_back(new Edge(n[3], n[4]));
+        edges.push_back(new Edge(n[5], n[4]));
+        edges.push_back(new Edge(n[3], n[6]));
+        edges.push_back(new Edge(n[3], n[0]));
+        edges.push_back(new Edge(n[3], n[7]));
         for (int i = 0; i < 8; i++) {
             unpassed[n[i]] = true;
         }
@@ -113,11 +115,11 @@ struct Graph {
         recursionBFS(startNode, q);
     }
 
-    vector<Edge*> kruskal() {
+    /**vector<Edge*> kruskal() {
         vector<Edge*> vt;
         sort(edges.begin(), edges.end(), soSanhNode);
         
-    }
+    }*/
 };
 
 int main()
